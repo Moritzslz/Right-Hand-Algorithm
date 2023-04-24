7397e044b686351e87d371e1e07d14ba739643d3
schultzm <schultzmoritz@gmail.com>
2023-04-24 17:23:20 +0200

    Nearly passing the example

--- src/gad/maze/Walker.java
+++ src/gad/maze/Walker.java
@@ -1,5 +1,7 @@
 package gad.maze;
 
+import java.util.ArrayList;
+
 public class Walker {
 
 	private boolean[][] maze;
@@ -13,59 +15,42 @@ public class Walker {
 	public boolean walk() {
 		int x = 1;
 		int y = 0;
-		boolean[][] blocked = maze;
-		boolean left = true;
-		boolean right = true;
-		boolean down = true;
-		boolean up = true;
-
-		result.addLocation(x, y);
-
+		result.addLocation(x,y);
 		for(int i = 0; i < maze.length * maze[0].length; i++) {
-			if (x == maze.length - 1 && y == maze[0].length - 2)
+			if(x == maze.length - 1 && y == maze[0].length - 2)
 				return true;
-
-			if(x < maze.length - 1)
-				right = blocked[x + 1][y];
-			if(y < maze[0].length - 1)
-				down = blocked[x][y + 1];
-			if(x > 0)
-				left = blocked[x - 1][y];
-			if(y > 0)
-				up = blocked[x][y - 1];
-
-
-			//If wall is right, left and in front go back
-			if (down && right && left) {
-				blocked[x][y] = true;
-				//y--;
-			}
-			//If wall is left, in front and above go back
-			else if (up && down && left) {
-				blocked[x][y] = true;
-				//x++;
-			}
-			//If wall is right, in front and above go back
-			 else if (down && right && up) {
-				blocked[x][y] = true;
-				//x--;
-			}
-			//If wall is right, left and above go back
-			else if (up && right && left) {
-				blocked[x][y] = true;
-				//y++;
-			}
-
-			if(!down)
+			if(maze[x-1][y] && !maze[x][y+1]) {
 				y++;
-			else if (!right)
+				result.addLocation(x,y);
+				if (!maze[x-1][y]) {
+					x--;
+					result.addLocation(x, y);
+				}
+			}
+			else if(maze[x][y+1] && !maze[x+1][y]) {
 				x++;
-			else if (!left)
-				x--;
-			else if (!up)
+				result.addLocation(x,y);
+				if (!maze[x][y+1]) {
+					y++;
+					result.addLocation(x,y);
+				}
+			}
+			else if(maze[x+1][y] && !maze[x][y-1]) {
 				y--;
-
-			result.addLocation(x, y);
+				result.addLocation(x,y);
+				if (!maze[x+1][y]) {
+					x++;
+					result.addLocation(x,y);
+				}
+			}
+			else if(maze[x][y-1] && !maze[x-1][y]) {
+				x--;
+				result.addLocation(x,y);
+				if (!maze[x][y-1]) {
+					y--;
+					result.addLocation(x, y);
+				}
+			}
 		}
 		return false;
 	}
